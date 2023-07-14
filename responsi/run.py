8898c9b8b3c0fcd0b28e6a230617d6749c700557
star2112 <bintangfadel73@gmail.com>
2023-07-14 18:42:38 +0700

    update

--- responsi/run.py
+++ responsi/run.py
@@ -1,4 +1,11 @@
-from responsi import app
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://user:userpass@mycluster.xntmcj2.mongodb.net/responsi?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    users = mongo.db.users.find({})
+    return render_template('index.html', users = users)
